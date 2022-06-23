cbb12c015c50e46c424cdca118885fae6cbac8d7
antibodyth <antibodyth01@gmail.com>
2022-06-23 15:31:37 +0700

    main

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
+import generateRoutes from 'virtual:generated-pages'
+import { setupLayouts} from 'virtual:generated-layouts'
+import 'windi.css'
+import App from './App.vue'
+
+const routes = setupLayouts(generateRoutes)
+const router = createRouter({
+  history : createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App).use(router)
+Object.values(import.meta.globEager('./modules/*.js')).map(i => i.install?.({app}))
+app.mount("#app")
