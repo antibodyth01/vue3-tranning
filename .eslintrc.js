1497a2128f8802c9a3edc6ce5b02d1255fbae24e
antibodyth <antibodyth01@gmail.com>
2022-06-23 17:25:09 +0700

    change .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,23 +11,21 @@ module.exports = {
   plugins:[
     'vue',
   ],
-  rules:{
+  rules: {
     'arrow-parens': 0,
-    'generator-star-spacing:': 0,
-    'no-debugger' : process-this.env.NODE_ENV === 'production' ? 2 : 0,
-    'comma-dangle': [2,'always-multiline'],
-    'space-before-function-paren':0,
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma-dangle': [2, 'always-multiline'],
+    'space-before-function-paren': 0,
     'object-curly-spacing': 0,
     'prefer-const': 0,
-    'no-unneeded-ternary': ['error', {
-      defaultAssigment: true}],
-      'vue/ma-attributes-per-line': 0,
-      'vue/attributes-order':0,
-      'no-control-regex':0,
-      'vue/multi-word-component-names':0,
-      'vue/html-self-closing':0,
-    },
-    globals:{
-
-    },
+    'no-unneeded-ternary': ["error", { "defaultAssignment": true }],
+    'vue/max-attributes-per-line': 0,
+    'vue/attributes-order': 0,
+    'vue/require-default-prop': 0,
+    'no-control-regex': 0,
+  },
+  globals: {
+    // '$': true,
+  },
 }
