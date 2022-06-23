490d47b961164c15f1dfb1f36649f8501c35430a
antibodyth <antibodyth01@gmail.com>
2022-06-23 17:25:59 +0700

    add template.vue

---
+++ src/pages/template.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-bold" :title="name">
+    Hello {{name}}
+  </div>
+  <div>Hello 2</div>
+
+  <button :disabled="lock">{{age}}</button>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name : 'P',
+      age : '27',
+      lock: false,
+    }
+  },
+}
+</script>
+
+<style>
+.text-blod{
+  font-weight: bold;
+}
+</style>
