9d606c33e25d191ff6d8c30a1a6c79cb30ad2695
antibodyth <antibodyth01@gmail.com>
2022-06-23 17:25:40 +0700

    add signin.vue

---
+++ src/pages/signin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form>
+      <div>Signin</div>
+      <div>
+        <label for="username">username</label>
+        <input type="text" id="username" />
+      </div>
+      <div>
+        <label for="password">password</label>
+        <input type="password" id="password" value="pass1" />
+      </div>
+      <div>
+        <input type="checkbox" id="remember"/>
+<label for="remember">Remember username?</label>
+      </div>
+<div>
+<button type="button">
+  Sign-In
+  </button>
+  <button type="reset">
+    Reset
+  </button>
+</div>
+    </form>
+  </div>
+</template>
